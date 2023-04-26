9d3e7df52f0d62d13d250639a95454b3d035161d
mzdrougos <105849937+mzdrougos@users.noreply.github.com>
2023-04-26 17:43:15 +0300

    Update forms.py

--- zoescatering/website/forms.py
+++ zoescatering/website/forms.py
@@ -1,7 +1,5 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import ProductReview
 
 
 class RegisterForm(UserCreationForm):
@@ -9,8 +7,3 @@ class RegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-
-class ReviewAdd(forms.ModelForm):
-    class Meta:
-        model=ProductReview
-        fields=('review_text','review_rating')

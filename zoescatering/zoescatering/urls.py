b3b670f81e45631edccabb5eb077857b71933a42
mzdrougos <105849937+mzdrougos@users.noreply.github.com>
2023-04-26 17:40:40 +0300

    Update urls.py

--- zoescatering/zoescatering/urls.py
+++ zoescatering/zoescatering/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('website/', include('website.urls')),
+    path('', include('website.urls')),
     path('admin/', admin.site.urls),
 ]

6b47b3b20d0ed673eb775eebf61d13a1854e71bd
mzdrougos <105849937+mzdrougos@users.noreply.github.com>
2023-04-26 17:38:18 +0300

    Update custom.js

--- zoescatering/website/static/website/custom.js
+++ zoescatering/website/static/website/custom.js
@@ -1,1 +1,7 @@
-...
+	$(".choose-size").on('click',function(){
+		$(".choose-size").removeClass('active');
+		$(this).addClass('active');
+
+		var _price=$(this).attr('data-price');
+		$(".product-price").text(_price);
+	})
